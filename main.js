b24a46442a64dc5912f5457cc87f73c011529ab7
manoj kota <kota.manoj8@gmail.com>
2021-10-05 14:35:25 -0500

    Update main.js

--- main.js
+++ main.js
@@ -32,3 +32,51 @@ var correct = 0;
      }
      i=i+1;
  }  
+ function power(){
+    b=document.getElementById("form1_base").value;
+    e=document.getElementById("form1_exponent").value;
+   output=document.getElementById("result1");
+    result="";
+    var c=b;
+    for(i=1;i<=e;i++){
+        c=c**i;
+        result=result+" "+c;
+ }
+ output.innerHTML = "powers are"+result;
+}
+function plus1(){
+    b=document.getElementById("form1_base").value;
+    e=document.getElementById("form1_exponent").value;
+    output1=document.getElementById("result2");
+   var  r=b+e;
+    output1.innerHTML =r;
+
+
+}
+function minus(){
+    b=document.getElementById("form1_base").value;
+    e=document.getElementById("form1_exponent").value;
+    output1=document.getElementById("result2");
+   let  r=b-e;
+    output1.innerHTML =r;
+
+
+}
+function times(){
+    b=document.getElementById("form1_base").value;
+    e=document.getElementById("form1_exponent").value;
+    output1=document.getElementById("result2");
+   let  r=b*e;
+    output1.innerHTML =r;
+
+
+}
+function divide(){
+    b=document.getElementById("form1_base").value;
+    e=document.getElementById("form1_exponent").value;
+    output1=document.getElementById("result2");
+   let  r=b/e;
+    output1.innerHTML =r;
+
+
+}
